6bdafb51131ad296c2fe52eba3855b79ebb1ae50
Guinet Valentin <valentin.guinet@epitech.eu>
2025-02-02 15:47:39 +0100

    [fix] jsx

--- epi_cards/vite.config.js
+++ epi_cards/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: '/EpiGAMES/', // Remplace par le nom exact de ton repo
   plugins: [react()],
   resolve: {
     alias: {
